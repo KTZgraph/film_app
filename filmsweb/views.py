81d535492b71eef54d94db656abf3b45b1c2167b
pawlaczyk <pawlaczyk@g.pl>
2020-05-16 18:59:14 +0200

    serializers

--- filmsweb/views.py
+++ filmsweb/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
-from .models import Film, AdditionalInfo, Rating, Actor
+from rest_framework import viewsets
+from django.contrib.auth.models import User
+
+from .models import Film, AdditionalInfo, Rating
 from .forms import FilmForm, AdditionalInfoForm, RatingForm
+from .serializers import UserSerializer, FilmSerializer
+
+
+class UserView(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class FilmView(viewsets.ModelViewSet):
+    queryset = Film.objects.all()
+    serializer_class =FilmSerializer
 
 
 def all_films(request):
